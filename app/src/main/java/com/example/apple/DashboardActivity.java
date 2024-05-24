a8741e730da731debda5f0e2604fc92ca16eb512
Khalid Khandaker <khalidkhandaker16@gmail.com>
2024-05-25 00:10:28 +0800

    Initial Commit

--- app/src/main/java/com/example/apple/DashboardActivity.java
+++ app/src/main/java/com/example/apple/DashboardActivity.java
@@ -1,14 +1,22 @@
 package com.example.apple;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.viewpager.widget.*;
+
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
@@ -16,12 +24,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    //Instantiate
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
     Button button;
 
     @Override
@@ -29,25 +43,55 @@ public class DashboardActivity extends AppCompatActivity {
         SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         View content = findViewById(android.R.id.content);
 
-
         super.onCreate(savedInstanceState);
-
-
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard);
-        button = (Button) findViewById(R.id.dashboard_button_logout);
+        //button = (Button) findViewById(R.id.dashboard_button_logout);
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.black));
         window.setNavigationBarColor(ContextCompat.getColor(this, R.color.black));
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(DashboardActivity.this, MainActivity.class));
-                finish();
-            }
-        });
+        //Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.dashboard_tool_bar_top);
+
+
+        //Toolbar
+        setSupportActionBar(toolbar);
+
+
+        //Actionbar
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        //Change the default action bar icon kasi hindi mapalitan ung color
+        setActionBarIcon();
+
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.bringToFront();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+      void setActionBarIcon() {
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.drawer_icon));
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return true;
     }
 }
