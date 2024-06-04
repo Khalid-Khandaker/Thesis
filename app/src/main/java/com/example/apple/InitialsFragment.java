bbde3612ba50ee7eb3c758c6f78334bb014fd580
Khalid Khandaker <khalidkhandaker16@gmail.com>
2024-06-04 12:01:12 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/apple/InitialsFragment.java
@@ -0,0 +1,18 @@
+package com.example.apple;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InitialsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_initials, container, false);
+    }
+}
--- app/src/main/java/com/example/apple/SimpleFragment.java
+++ app/src/main/java/com/example/apple/SimpleFragment.java
@@ -7,16 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-public class PracticeFragment extends Fragment {
-    TextView textview;
+public class SimpleFragment extends Fragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_simple, container, false);
 
-        
+
+
         return view;
     }
 }
